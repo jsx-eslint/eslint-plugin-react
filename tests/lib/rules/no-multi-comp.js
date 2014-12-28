fda9f8f5dd1111e650bccbc664f08b3a9feb4554
Yannick Croissant <yc@deezer.com>
2014-12-28 18:28:47 +0100

    Add rules tests

---
+++ tests/lib/rules/no-multi-comp.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview Prevent multiple component definition per file
+ * @author Yannick Croissant
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var eslint = require('eslint').linter;
+var ESLintTester = require('eslint-tester');
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+var eslintTester = new ESLintTester(eslint);
+eslintTester.addRuleTest('lib/rules/no-multi-comp', {
+
+    valid: [
+        {
+            code: 'var Hello = require(\'./components/Hello\');var HelloJohn = React.createClass({render: function() {return <Hello name="John" />;}});',
+            settings: {
+                ecmascript: 6,
+                jsx: true
+            }
+        }
+    ],
+
+    invalid: [
+        {
+            code: 'var Hello = React.createClass({render: function() {return <div>Hello {this.props.name}</div>;}});var HelloJohn = React.createClass({render: function() {return <Hello name="John" />;}});',
+            settings: {
+                ecmascript: 6,
+                jsx: true
+            },
+            errors: [{
+                message: 'Declare only one React component per file'
+            }]
+        }
+    ]
+});
---
+++ tests/lib/rules/prop-types.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview Prevent missing propTypes in a React component definition
+ * @author Yannick Croissant
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var eslint = require('eslint').linter;
+var ESLintTester = require('eslint-tester');
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+var eslintTester = new ESLintTester(eslint);
+eslintTester.addRuleTest('lib/rules/prop-types', {
+
+    valid: [
+        {
+            code: 'var Hello = React.createClass({propTypes: {name: React.PropTypes.string.isRequired},render: function() {return <div>Hello {this.props.name}</div>;}});',
+            settings: {
+                ecmascript: 6,
+                jsx: true
+            }
+        }
+    ],
+
+    invalid: [
+        {
+            code: 'var Hello = React.createClass({render: function() {return <div>Hello {this.props.name}</div>;}});',
+            settings: {
+                ecmascript: 6,
+                jsx: true
+            },
+            errors: [{
+                message: 'Component definition is missing props validation'
+            }]
+        }
+    ]
+});
