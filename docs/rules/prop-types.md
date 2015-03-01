df4cf52235459ca0e358b73065576e0e29813746
Yannick Croissant <yannick.croissant@gmail.com>
2015-03-02 00:16:53 +0100

    Update prop-types rule to check props usage (fixes #4)

--- docs/rules/prop-types.md
+++ docs/rules/prop-types.md
@@ -1,4 +1,4 @@
-# Prevent missing propTypes in a React component definition (prop-types)
+# Prevent missing props validation in a React component definition (prop-types)
 
 PropTypes improve the reusability of your component by validating the received data.
 
@@ -14,11 +14,26 @@ var Hello = React.createClass({
     return <div>Hello {this.props.name}</div>;
   }
 });
+
+var Hello = React.createClass({
+  propTypes: {
+    fistname: React.PropTypes.string.isRequired
+  },
+  render: function() {
+    return <div>Hello {this.props.fistname} {this.props.lastname}</div>; // lastname type is not defined in propTypes
+  }
+});
 ```
 
 The following patterns are not considered warnings:
 
 ```js
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello World</div>;
+  }
+});
+
 var Hello = React.createClass({
   propTypes: {
     name: React.PropTypes.string.isRequired
--- tests/lib/rules/prop-types.js
+++ tests/lib/rules/prop-types.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Prevent missing propTypes in a React component definition
+ * @fileoverview Prevent missing props validation in a React component definition
  * @author Yannick Croissant
  */
 'use strict';
@@ -32,6 +32,29 @@ eslintTester.addRuleTest('lib/rules/prop-types', {
             ecmaFeatures: {
               jsx: true
             }
+        }, {
+            code: '\
+              var Hello = React.createClass({\
+                propTypes: {\
+                  name: React.PropTypes.object.isRequired\
+                },\
+                render: function() {\
+                  return <div>Hello {this.props.name.firstname}</div>;\
+                }\
+              });',
+            ecmaFeatures: {
+              jsx: true
+            }
+        }, {
+            code: '\
+              var Hello = React.createClass({\
+                render: function() {\
+                  return <div>Hello World</div>;\
+                }\
+              });',
+            ecmaFeatures: {
+              jsx: true
+            }
         }
     ],
 
@@ -47,7 +70,30 @@ eslintTester.addRuleTest('lib/rules/prop-types', {
               jsx: true
             },
             errors: [{
-                message: 'Component definition is missing props validation'
+                message: '\'name\' is missing in props validation'
+            }]
+        }, {
+            code: '\
+              var Hello = React.createClass({\
+                propTypes: {\
+                  name: React.PropTypes.string.isRequired\
+                },\
+                render: function() {\
+                  return <div>Hello {this.props.name} and {this.props.propWithoutTypeDefinition}</div>;\
+                }\
+              });\
+              var Hello2 = React.createClass({\
+                render: function() {\
+                  return <div>Hello {this.props.name}</div>;\
+                }\
+              });',
+            ecmaFeatures: {
+              jsx: true
+            },
+            errors: [{
+                message: '\'propWithoutTypeDefinition\' is missing in props validation'
+            }, {
+                message: '\'name\' is missing in props validation'
             }]
         }
     ]
