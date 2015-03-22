e5460088fe761df0255b2f287052a3ac482f0cc9
Yannick Croissant <yannick.croissant@gmail.com>
2015-03-22 19:32:24 +0000

    Update coding style rules

--- tests/index.js
+++ tests/index.js
@@ -8,23 +8,23 @@ var fs = require('fs');
 var path = require('path');
 
 var rules = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
-    .map(function(f) {
-        return path.basename(f, '.js');
-    });
+  .map(function(f) {
+    return path.basename(f, '.js');
+  });
 
 describe('all rule files should be exported by the plugin', function() {
-    rules.forEach(function(ruleName) {
-        it('should export ' + ruleName, function() {
-            assert.equal(
-                plugin.rules[ruleName],
-                require(path.join('../lib/rules', ruleName))
-            );
-        });
-        it('should configure ' + ruleName + ' off by default', function() {
-            assert.equal(
-                plugin.rulesConfig[ruleName],
-                0
-            );
-        });
+  rules.forEach(function(ruleName) {
+    it('should export ' + ruleName, function() {
+      assert.equal(
+        plugin.rules[ruleName],
+        require(path.join('../lib/rules', ruleName))
+      );
     });
+    it('should configure ' + ruleName + ' off by default', function() {
+      assert.equal(
+        plugin.rulesConfig[ruleName],
+        0
+      );
+    });
+  });
 });
