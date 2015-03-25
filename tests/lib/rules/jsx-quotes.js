7e510e5f8292554112f0c7957a2c80ccb0b8532c
Yannick Croissant <yannick.croissant@gmail.com>
2015-03-25 00:31:23 +0000

    Fix jsx-quotes on Literal childs (fixes #29)

--- tests/lib/rules/jsx-quotes.js
+++ tests/lib/rules/jsx-quotes.js
@@ -22,7 +22,8 @@ eslintTester.addRuleTest('lib/rules/jsx-quotes', {
         {code: '<App foo=\'bar\' />;', args: [1, 'single'], ecmaFeatures: {jsx: true}},
         {code: '<App foo="bar" />;', args: [1, 'double'], ecmaFeatures: {jsx: true}},
         {code: '<App foo="ba\'r" />;', args: [1, 'single', 'avoid-escape'], ecmaFeatures: {jsx: true}},
-        {code: '<App foo=\'ba"r\' />;', args: [1, 'double', 'avoid-escape'], ecmaFeatures: {jsx: true}}
+        {code: '<App foo=\'ba"r\' />;', args: [1, 'double', 'avoid-escape'], ecmaFeatures: {jsx: true}},
+        {code: '<App>foo</App>;', args: [1, 'single'], ecmaFeatures: {jsx: true}}
     ],
     invalid: [
         {code: '<App foo="bar" />;',
