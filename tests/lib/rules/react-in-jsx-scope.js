4cbe94636308f51cb1d415b3f0ba3e79dda3f3f7
Nicola Molinari <nicola.molinari@commercetools.de>
2015-04-10 14:53:06 +0200

    Reproduce error using ES6 imports

--- tests/lib/rules/react-in-jsx-scope.js
+++ tests/lib/rules/react-in-jsx-scope.js
@@ -26,7 +26,20 @@ eslintTester.addRuleTest('lib/rules/react-in-jsx-scope', {
         {code: 'var React, App, a=1; function elem() { return <App attr={a} />; }', ecmaFeatures: {jsx: true}},
         {code: 'var React, App; <App />;', args: [1, {vars: 'all'}], ecmaFeatures: {globalReturn: true, jsx: true}},
         {code: '/** @jsx Foo */ var Foo, App; <App />;', args: [1, {vars: 'all'}], ecmaFeatures: {jsx: true}},
-        {code: '/** @jsx Foo.Bar */ var Foo, App; <App />;', args: [1, {vars: 'all'}], ecmaFeatures: {jsx: true}}
+        {code: '/** @jsx Foo.Bar */ var Foo, App; <App />;', args: [1, {vars: 'all'}], ecmaFeatures: {jsx: true}},
+        {code: [
+            'import React from \'react/addons\';',
+            'const Button = React.createClass({',
+            '  render() {',
+            '    return (',
+            '      <button {...this.props}>{this.props.children}</button>',
+            '    )',
+            '  }',
+            '});',
+            'export default Button;'
+          ].join('\n'),
+          ecmaFeatures: {blockBindings: true, objectLiteralShorthandMethods: true, modules: true, jsx: true}
+        }
     ],
     invalid: [
         {code: 'var App, a = <App />;',
