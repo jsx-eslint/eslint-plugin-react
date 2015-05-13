bb96d3cb2e88f4da3d60b04d67e7a064de706688
Yannick Croissant <yannick.croissant@gmail.com>
2015-05-13 21:53:17 +0000

    Reformat no-did-mount-set-state and no-did-update-set-state tests

--- tests/lib/rules/no-did-mount-set-state.js
+++ tests/lib/rules/no-did-mount-set-state.js
@@ -18,64 +18,64 @@ var ESLintTester = require('eslint-tester');
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest('lib/rules/no-did-mount-set-state', {
 
-    valid: [
-        {
-            code: '\
-              var Hello = React.createClass({\
-                render: function() {\
-                  return <div>Hello {this.props.name}</div>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            }
-        }, {
-            code: '\
-              var Hello = React.createClass({\
-                componentDidMount: function() {},\
-                render: function() {\
-                  return <div>Hello {this.props.name}</div>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            }
-        }, {
-            code: '\
-              var Hello = React.createClass({\
-                componentDidMount: function() {\
-                  someNonMemberFunction(arg);\
-                  this.someHandler = this.setState;\
-                },\
-                render: function() {\
-                  return <div>Hello {this.props.name}</div>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            }
-        }
-    ],
+  valid: [{
+    code: [
+      'var Hello = React.createClass({',
+      '  render: function() {',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    ecmaFeatures: {
+      jsx: true
+    }
+  }, {
+    code: [
+      'var Hello = React.createClass({',
+      'componentDidMount: function() {},',
+      '  render: function() {',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    ecmaFeatures: {
+      jsx: true
+    }
+  }, {
+    code: [
+      'var Hello = React.createClass({',
+      '  componentDidMount: function() {',
+      '    someNonMemberFunction(arg);',
+      '    this.someHandler = this.setState;',
+      '  },',
+      '  render: function() {',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    ecmaFeatures: {
+      jsx: true
+    }
+  }],
 
-    invalid: [
-        {
-            code: '\
-              var Hello = React.createClass({\
-                componentDidMount: function() {\
-                  this.setState({\
-                    name: this.props.name.toUpperCase()\
-                  });\
-                },\
-                render: function() {\
-                  return <div>Hello {this.state.name}</div>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            },
-            errors: [{
-                message: 'Do not use setState in componentDidMount'
-            }]
-        }
-    ]
+  invalid: [{
+    code: [
+      'var Hello = React.createClass({',
+      '  componentDidMount: function() {',
+      '    this.setState({',
+      '      name: this.props.name.toUpperCase()',
+      '    });',
+      '  },',
+      '  render: function() {',
+      '    return <div>Hello {this.state.name}</div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    ecmaFeatures: {
+      jsx: true
+    },
+    errors: [{
+      message: 'Do not use setState in componentDidMount'
+    }]
+  }]
 });
