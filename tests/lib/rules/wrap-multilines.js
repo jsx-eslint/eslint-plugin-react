3610e9d3c513519aa690709da3d69e28ce459ec0
Benjamin Woodruff <bgw@fb.com>
2015-06-02 16:15:38 -0700

    Make wrap-multilines more granular
    This adds an option for enabling/disabling wrap-multilines in the
    context of the enclosing expression. In facebook/react, we only care
    about jsx expressions in return statements:
    
    https://github.com/facebook/react/pull/4007#discussion_r31554866

--- tests/lib/rules/wrap-multilines.js
+++ tests/lib/rules/wrap-multilines.js
@@ -12,6 +12,67 @@ var eslint = require('eslint').linter;
 var ESLintTester = require('eslint-tester');
 
 // ------------------------------------------------------------------------------
+// Code Snippets
+// ------------------------------------------------------------------------------
+
+var RETURN_SINGLE_LINE = '\
+  var Hello = React.createClass({\
+    render: function() {\
+      return <p>Hello {this.props.name}</p>;\
+    }\
+  });';
+
+var RETURN_PAREN = '\
+  var Hello = React.createClass({\
+    render: function() {\
+      return (\n\
+        <div>\n\
+          <p>Hello {this.props.name}</p>\n\
+        </div>\n\
+      );\
+    }\
+  });';
+
+var RETURN_NO_PAREN = '\
+  var Hello = React.createClass({\
+    render: function() {\
+      return <div>\n\
+        <p>Hello {this.props.name}</p>\n\
+      </div>;\
+    }\
+  });';
+
+var DECLARATION_SINGLE_LINE = 'var hello = <p>Hello</p>;';
+
+var DECLARATION_PAREN = '\
+  var hello = (\n\
+    <div>\n\
+      <p>Hello</p>\n\
+    </div>\n\
+  );';
+
+var DECLARATION_NO_PAREN = '\
+  var hello = <div>\n\
+    <p>Hello</p>\n\
+  </div>;';
+
+var ASSIGNMENT_SINGLE_LINE = 'var hello; hello = <p>Hello</p>;';
+
+var ASSIGNMENT_PAREN = '\
+  var hello;\
+  hello = (\n\
+    <div>\n\
+      <p>Hello</p>\n\
+    </div>\n\
+  );';
+
+var ASSIGNMENT_NO_PAREN = '\
+  var hello;\
+  hello = <div>\n\
+    <p>Hello</p>\n\
+  </div>;';
+
+// ------------------------------------------------------------------------------
 // Tests
 // ------------------------------------------------------------------------------
 
@@ -20,97 +81,67 @@ eslintTester.addRuleTest('lib/rules/wrap-multilines', {
 
     valid: [
         {
-            code: '\
-              var Hello = React.createClass({\
-                render: function() {\
-                  return <p>Hello {this.props.name}</p>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            }
+            code: RETURN_SINGLE_LINE,
+            ecmaFeatures: {jsx: true}
         }, {
-            code: '\
-              var Hello = React.createClass({\
-                render: function() {\
-                  return (\n\
-                    <div>\n\
-                      <p>Hello {this.props.name}</p>\n\
-                    </div>\n\
-                  );\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            }
+            code: RETURN_PAREN,
+            ecmaFeatures: {jsx: true}
         }, {
-            code: 'var hello = <p>Hello</p>;',
-            ecmaFeatures: {
-              jsx: true
-            }
+            code: RETURN_NO_PAREN,
+            args: [1, {return: false}],
+            ecmaFeatures: {jsx: true}
         }, {
-            code: '\
-              var hello = (\n\
-                <div>\n\
-                  <p>Hello</p>\n\
-                </div>\n\
-              );',
-            ecmaFeatures: {
-              jsx: true
-            }
+            code: DECLARATION_SINGLE_LINE,
+            ecmaFeatures: {jsx: true}
         }, {
-            code: '\
-              var hello;\
-              hello = (\n\
-                <div>\n\
-                  <p>Hello</p>\n\
-                </div>\n\
-              );',
-            ecmaFeatures: {
-              jsx: true
-            }
+            code: DECLARATION_PAREN,
+            ecmaFeatures: {jsx: true}
+        }, {
+            code: DECLARATION_NO_PAREN,
+            args: [1, {declaration: false}],
+            ecmaFeatures: {jsx: true}
+        }, {
+            code: ASSIGNMENT_SINGLE_LINE,
+            args: [1, {declaration: false}],
+            ecmaFeatures: {jsx: true}
+        }, {
+            code: ASSIGNMENT_PAREN,
+            ecmaFeatures: {jsx: true}
+        }, {
+            code: ASSIGNMENT_NO_PAREN,
+            args: [1, {assignment: false}],
+            ecmaFeatures: {jsx: true}
         }
     ],
 
     invalid: [
         {
-            code: '\
-              var Hello = React.createClass({\
-                render: function() {\
-                  return <div>\n\
-                    <p>Hello {this.props.name}</p>\n\
-                  </div>;\
-                }\
-              });',
-            ecmaFeatures: {
-              jsx: true
-            },
-            errors: [{
-                message: 'Missing parentheses around multilines JSX'
-            }]
+            code: RETURN_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
         }, {
-            code: '\
-              var hello = <div>\n\
-                <p>Hello</p>\n\
-              </div>;',
-            ecmaFeatures: {
-              jsx: true
-            },
-            errors: [{
-                message: 'Missing parentheses around multilines JSX'
-            }]
+            code: RETURN_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            args: [1, {return: true}],
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
         }, {
-            code: '\
-              var hello;\
-              hello = <div>\n\
-                <p>Hello</p>\n\
-              </div>;',
-            ecmaFeatures: {
-              jsx: true
-            },
-            errors: [{
-                message: 'Missing parentheses around multilines JSX'
-            }]
+            code: DECLARATION_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
+        }, {
+            code: DECLARATION_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            args: [1, {declaration: true}],
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
+        }, {
+            code: ASSIGNMENT_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
+        }, {
+            code: ASSIGNMENT_NO_PAREN,
+            ecmaFeatures: {jsx: true},
+            args: [1, {assignment: true}],
+            errors: [{message: 'Missing parentheses around multilines JSX'}]
         }
     ]
 });
