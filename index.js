ad68409c9d24c5c3a1ba98905e946b9f787a9db5
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-04-07 22:50:37 +0300

    534 - Add a new rule that ensures the position of the first property for JSX components.

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ module.exports = {
     'jsx-key': require('./lib/rules/jsx-key'),
     'no-string-refs': require('./lib/rules/no-string-refs'),
     'prefer-stateless-function': require('./lib/rules/prefer-stateless-function'),
-    'require-render-return': require('./lib/rules/require-render-return')
+    'require-render-return': require('./lib/rules/require-render-return'),
+    'jsx-first-prop-new-line': require('./lib/rules/jsx-first-prop-new-line')
   },
   configs: {
     recommended: {
