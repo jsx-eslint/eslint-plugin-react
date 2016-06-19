16e7019ec669a6dc87775afd54e267e583b74397
Ethan Cohen <ethanvcohen@gmail.com>
2016-06-19 12:00:37 -0700

    Add support for function-bind to jsx-no-bind (fixes #532)

--- tests/lib/rules/jsx-no-bind.js
+++ tests/lib/rules/jsx-no-bind.js
@@ -142,6 +142,23 @@ ruleTester.run('jsx-no-bind', rule, {
       code: '<div ref={c => this._input = c}></div>',
       errors: [{message: 'JSX props should not use arrow functions'}],
       parser: 'babel-eslint'
+    },
+
+    // Bind expression
+    {
+      code: '<div foo={::this.onChange} />',
+      errors: [{message: 'JSX props should not use ::'}],
+      parser: 'babel-eslint'
+    },
+    {
+      code: '<div foo={foo.bar::baz} />',
+      errors: [{message: 'JSX props should not use ::'}],
+      parser: 'babel-eslint'
+    },
+    {
+      code: '<div foo={foo::bar} />',
+      errors: [{message: 'JSX props should not use ::'}],
+      parser: 'babel-eslint'
     }
   ]
 });
