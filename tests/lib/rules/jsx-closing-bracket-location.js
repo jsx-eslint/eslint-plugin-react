aa94a5f4dc6119ea8fa4adcab80cbd46a108ec05
Patrick Hayes <pfhayes@gmail.com>
2016-07-17 21:45:30 -0700

    Add test that fails on master

--- tests/lib/rules/jsx-closing-bracket-location.js
+++ tests/lib/rules/jsx-closing-bracket-location.js
@@ -653,6 +653,38 @@ ruleTester.run('jsx-closing-bracket-location', rule, {
     }]
   }, {
     code: [
+      'const Button = function(props) {',
+      '  return (',
+      '    <Button',
+      '      size={size}',
+      '      onClick={onClick}',
+      '                                    >',
+      '      Button Text',
+      '    </Button>',
+      '  );',
+      '};'
+    ].join('\n'),
+    output: [
+      'const Button = function(props) {',
+      '  return (',
+      '    <Button',
+      '      size={size}',
+      '      onClick={onClick}',
+      '      >',
+      '      Button Text',
+      '    </Button>',
+      '  );',
+      '};'
+    ].join('\n'),
+    options: ['props-aligned'],
+    parserOptions: parserOptions,
+    errors: [{
+      message: messageWithDetails(MESSAGE_PROPS_ALIGNED, 7, false),
+      line: 6,
+      column: 37
+    }]
+  }, {
+    code: [
       '<Provider',
       '  store',
       '  >',
