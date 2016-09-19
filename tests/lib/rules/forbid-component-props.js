44345f62ec12baa99d61130fdf66044abcce83dc
Edward Drapkin <edward@colorjar.com>
2016-09-18 23:05:46 -0400

    New API per request + tested

--- tests/lib/rules/forbid-component-props.js
+++ tests/lib/rules/forbid-component-props.js
@@ -26,6 +26,8 @@ require('babel-eslint');
 
 var CLASSNAME_ERROR_MESSAGE = 'Prop `className` is forbidden on Components';
 var STYLE_ERROR_MESSAGE = 'Prop `style` is forbidden on Components';
+var DOM_STYLE_ERROR_MESSAGE = 'Prop `style` is forbidden on DOM nodes';
+var TAG_ERROR_MESSAGE = 'Prop `style` is specifically forbidden on tags named i';
 
 var ruleTester = new RuleTester();
 ruleTester.run('forbid-component-props', rule, {
@@ -81,7 +83,52 @@ ruleTester.run('forbid-component-props', rule, {
     ].join('\n'),
     options: [{forbid: ['style', 'foo']}],
     parserOptions: parserOptions
-  }],
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <Foo className="bar" />;',
+      '  }',
+      '});'
+    ].join('\n'),
+    options: [{forbid: ['classNames'], defaultAllowOnComponents: false}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <Foo className="bar" />;',
+      '  }',
+      '});'
+    ].join('\n'),
+    options: [{forbid: [{property: 'className', allowOnComponents: true}]}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <div className="bar"><Foo className="bar" /></div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    options: [{forbid: [{property: 'className', allowOnComponents: true, allowOnDOM: true}]}],
+    parserOptions: parserOptions
+  },
+    {
+      code: [
+        'var First = React.createClass({',
+        '  propTypes: externalPropTypes,',
+        '  render: function() {',
+        '    return <div className="bar"><Foo className="bar" /></div>;',
+        '  }',
+        '});'
+      ].join('\n'),
+      options: [{forbid: [{property: 'className', allow: ['div', 'Foo']}], defaultAllowOnDOM: false, defaultAllowOnComponents: false}],
+      parserOptions: parserOptions
+    }],
 
   invalid: [{
     code: [
@@ -143,11 +190,70 @@ ruleTester.run('forbid-component-props', rule, {
     ].join('\n'),
     parserOptions: parserOptions,
     options: [{forbid: ['className', 'style']}],
-    errors: [{
-      message: STYLE_ERROR_MESSAGE,
-      line: 4,
-      column: 17,
-      type: 'JSXAttribute'
-    }]
+    errors: [
+      {
+        message: STYLE_ERROR_MESSAGE,
+        line: 4,
+        column: 17,
+        type: 'JSXAttribute'
+      }
+    ]
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <Foo style={{color: "red"}} />;',
+      '  }',
+      '});'
+    ].join('\n'),
+    parserOptions: parserOptions,
+    options: [{forbid: ['className', {property: 'style', allowOnComponents: false}], defaultAllowOnComponents: true}],
+    errors: [
+      {
+        message: STYLE_ERROR_MESSAGE,
+        line: 4,
+        column: 17,
+        type: 'JSXAttribute'
+      }
+    ]
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <Foo><i style={{color: "red"}} /></Foo>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    parserOptions: parserOptions,
+    options: [{forbid: ['className', {property: 'style', forbid: ['i']}], defaultAllowOnDOM: true}],
+    errors: [
+      {
+        message: TAG_ERROR_MESSAGE,
+        line: 4,
+        column: 20,
+        type: 'JSXAttribute'
+      }
+    ]
+  }, {
+    code: [
+      'var First = React.createClass({',
+      '  propTypes: externalPropTypes,',
+      '  render: function() {',
+      '    return <Foo><i style={{color: "red"}} /></Foo>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    parserOptions: parserOptions,
+    options: [{forbid: ['className', 'style'], defaultAllowOnDOM: false}],
+    errors: [
+      {
+        message: DOM_STYLE_ERROR_MESSAGE,
+        line: 4,
+        column: 20,
+        type: 'JSXAttribute'
+      }
+    ]
   }]
 });
