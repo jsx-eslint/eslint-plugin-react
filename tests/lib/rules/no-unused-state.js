e8205386a2c0234793621c21f4612c3acac23fd2
William Binns-Smith <wbinnssmith@gmail.com>
2017-03-07 12:20:53 -0800

    Add no-unused-state
    This adds a new rule, react/no-unused-state, which discovers state fields in a React component and warns if any of them are never read. It was developed internally at Facebook by @rjbailey and has been in use throughout Facebook's JS for a couple months now. It was written against a modern version of node, so has been rebased on @jlharb's branch dropping support for node < 4.
    
    It currently supports es2015 classes extending `React.Component`  (no support currently for `React.createClass()`) and can detect when state is as `this.state = {}`, assigning state in a property initializer, and when calling `this.setState()`.

---
+++ tests/lib/rules/no-unused-state.js
@@ -0,0 +1,414 @@
+/**
+ * @fileoverview Tests for no-unused-state
+ */
+
+'use strict';
+
+const rule = require('../../../lib/rules/no-unused-state');
+const RuleTester = require('eslint').RuleTester;
+
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true
+  }
+};
+
+const eslintTester = new RuleTester({parserOptions, parser: 'babel-eslint'});
+
+function getErrorMessages(unusedFields) {
+  return unusedFields.map(field => ({
+    message: `Unused state field: '${field}'`
+  }));
+}
+
+eslintTester.run('no-unused-state', rule, {
+  valid: [
+    `
+      class NoStateTest extends React.Component {
+        render() {
+          return <SomeComponent />;
+        }
+      }
+    `,
+    `
+      class CtorStateTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          return <SomeComponent foo={this.state.foo} />;
+        }
+      }
+    `,
+    `
+      class SetStateTest extends React.Component {
+        onFooChange(newFoo) {
+          this.setState({ foo: newFoo });
+        }
+        render() {
+          return <SomeComponent foo={this.state.foo} />;
+        }
+      }
+    `,
+    `
+      class ClassPropertyStateTest extends React.Component {
+        state = { foo: 0 };
+        render() {
+          return <SomeComponent foo={this.state.foo} />;
+        }
+      }
+    `,
+    `
+      class VariableDeclarationTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const foo = this.state.foo;
+          return <SomeComponent foo={foo} />;
+        }
+      }
+    `,
+    `
+      class DestructuringTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const {foo: myFoo} = this.state;
+          return <SomeComponent foo={myFoo} />;
+        }
+      }
+    `,
+    `
+      class ShorthandDestructuringTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const {foo} = this.state;
+          return <SomeComponent foo={foo} />;
+        }
+      }
+    `,
+    `
+      class AliasDeclarationTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const state = this.state;
+          return <SomeComponent foo={state.foo} />;
+        }
+      }
+    `,
+    `
+      class AliasAssignmentTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          let state;
+          state = this.state;
+          return <SomeComponent foo={state.foo} />;
+        }
+      }
+    `,
+    `
+      class DestructuringAliasTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const {state: myState} = this;
+          return <SomeComponent foo={myState.foo} />;
+        }
+      }
+    `,
+    `
+      class ShorthandDestructuringAliasTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const {state} = this;
+          return <SomeComponent foo={state.foo} />;
+        }
+      }
+    `,
+    `
+      class RestPropertyTest extends React.Component {
+        constructor() {
+          this.state = {
+            foo: 0,
+            bar: 1,
+          };
+        }
+        render() {
+          const {foo, ...others} = this.state;
+          return <SomeComponent foo={foo} bar={others.bar} />;
+        }
+      }
+    `,
+    `
+      class DeepDestructuringTest extends React.Component {
+        state = { foo: 0, bar: 0 };
+        render() {
+          const {state: {foo, ...others}} = this;
+          return <SomeComponent foo={foo} bar={others.bar} />;
+        }
+      }
+    `,
+    // A cleverer analysis might recognize that the following should be errors,
+    // but they're out of scope for this lint rule.
+    `
+      class MethodArgFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        consumeFoo(foo) {}
+        render() {
+          this.consumeFoo(this.state.foo)
+          return <SomeComponent />;
+        }
+      }
+    `,
+    `
+      class AssignedToObjectFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const obj = { foo: this.state.foo, bar: 0 };
+          return <SomeComponent bar={obj.bar} />;
+        }
+      }
+    `,
+    `
+      class ComputedAccessFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0, bar: 1 };
+        }
+        render() {
+          const bar = 'bar';
+          return <SomeComponent bar={this.state[bar]} />;
+        }
+      }
+    `,
+    `
+      class JsxSpreadFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          return <SomeComponent {...this.state} />;
+        }
+      }
+    `,
+    `
+      class AliasedJsxSpreadFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const state = this.state;
+          return <SomeComponent {...state} />;
+        }
+      }
+    `,
+    `
+      class ObjectSpreadFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const attrs = { ...this.state, foo: 1 };
+          return <SomeComponent foo={attrs.foo} />;
+        }
+      }
+    `,
+    `
+      class ShadowingFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          const state = this.state;
+          let foo;
+          {
+            const state = { foo: 5 };
+            foo = state.foo;
+          }
+          return <SomeComponent foo={foo} />;
+        }
+      }
+    `,
+    `
+      class TypeCastExpressionSpreadFalseNegativeTest extends React.Component {
+        constructor() {
+          this.state = { foo: 0 };
+        }
+        render() {
+          return <SomeComponent {...(this.state: any)} />;
+        }
+      }
+    `
+  ],
+
+  invalid: [
+    {
+      code: `
+        class UnusedCtorStateTest extends React.Component {
+          constructor() {
+            this.state = { foo: 0 };
+          }
+          render() {
+            return <SomeComponent />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class UnusedSetStateTest extends React.Component {
+          onFooChange(newFoo) {
+            this.setState({ foo: newFoo });
+          }
+          render() {
+            return <SomeComponent />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class UnusedClassPropertyStateTest extends React.Component {
+          state = { foo: 0 };
+          render() {
+            return <SomeComponent />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class UnusedStateWhenPropsAreSpreadTest extends React.Component {
+          constructor() {
+            this.state = { foo: 0 };
+          }
+          render() {
+            return <SomeComponent {...this.props} />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class AliasOutOfScopeTest extends React.Component {
+          constructor() {
+            this.state = { foo: 0 };
+          }
+          render() {
+            const state = this.state;
+            return <SomeComponent />;
+          }
+          someMethod() {
+            const outOfScope = state.foo;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class MultipleErrorsTest extends React.Component {
+          constructor() {
+            this.state = {
+              foo: 0,
+              bar: 1,
+              baz: 2,
+              qux: 3,
+            };
+          }
+          render() {
+            let {state} = this;
+            return <SomeComponent baz={state.baz} qux={state.qux} />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo', 'bar'])
+    },
+    {
+      code: `
+        class MultipleErrorsForSameKeyTest extends React.Component {
+          constructor() {
+            this.state = { foo: 0 };
+          }
+          onFooChange(newFoo) {
+            this.setState({ foo: newFoo });
+          }
+          render() {
+            return <SomeComponent />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo', 'foo'])
+    },
+    {
+      code: `
+        class UnusedRestPropertyFieldTest extends React.Component {
+          constructor() {
+            this.state = {
+              foo: 0,
+              bar: 1,
+            };
+          }
+          render() {
+            const {bar, ...others} = this.state;
+            return <SomeComponent bar={bar} />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['foo'])
+    },
+    {
+      code: `
+        class TypeCastExpressionTest extends React.Component {
+          constructor() {
+            this.state = {
+              foo: 0,
+              bar: 1,
+              baz: 2,
+              qux: 3,
+            };
+          }
+          render() {
+            const foo = ((this: any).state: any).foo;
+            const {bar, ...others} = (this.state: any);
+            let baz;
+            baz = (others: any)['baz'];
+            return <SomeComponent foo={foo} bar={bar} baz={baz} />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['qux'])
+    },
+    {
+      code: `
+        class UnusedDeepDestructuringTest extends React.Component {
+          state = { foo: 0, bar: 0 };
+          render() {
+            const {state: {foo}} = this;
+            return <SomeComponent foo={foo} />;
+          }
+        }
+      `,
+      errors: getErrorMessages(['bar'])
+    }
+  ]
+});
