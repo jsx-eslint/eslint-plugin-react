03a806699291ff762ffad98c5d19fba8fec3c62a
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-15 23:31:33 +0200

    fix: fix the previous failing test

--- tests/lib/rules/void-dom-elements-no-children.js
+++ tests/lib/rules/void-dom-elements-no-children.js
@@ -63,28 +63,25 @@ ruleTester.run('void-dom-elements-no-children', rule, {
         'import React from "react";',
         'const { createElement } = React;',
         'createElement("div")'
-      ].join('\n'),
-      parser: 'babel-eslint'
+      ].join('\n')
     }, {
       code: [
         'import React from "react";',
         'const { createElement } = React;',
         'createElement("img")'
-      ].join('\n'),
-      parser: 'babel-eslint'
+      ].join('\n')
     }, {
       code: [
         'import React, {createElement, PureComponent} from \'react\';',
         'class Button extends PureComponent {',
-        '  handleClick = ev => {',
+        '  handleClick(ev) {',
         '    ev.preventDefault();',
         '  }',
         '  render() {',
         '    return <div onClick={this.handleClick}>Hello</div>;',
         '  }',
         '}'
-      ].join('\n'),
-      parser: 'babel-eslint'
+      ].join('\n')
     }
   ],
   invalid: [
