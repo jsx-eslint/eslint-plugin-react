d5e5a2e716bf6a3b0105273469534cc8fa01cee9
jackyho112 <jackyho112dev@gmail.com>
2017-08-07 16:46:19 -0700

    Support adding ability to enfore curly brace presence

---
+++ tests/lib/rules/jsx-curly-brace-presence.js
@@ -0,0 +1,110 @@
+/**
+ * @fileoverview Enfore curly brace presence or disallow unnecessary curly brace in JSX
+ * @author Jacky Ho
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/jsx-curly-brace-presence');
+const RuleTester = require('eslint').RuleTester;
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true
+  }
+};
+
+const missingCurlyMessage = 'Need to wrap this literal in a JSX expression.';
+const unnecessaryCurlyMessage = 'Curly braces are unnecessary here.';
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions});
+ruleTester.run('jsx-curly-spacing', rule, {
+  valid: [
+    {
+      code: "<App>{<myApp></myApp>}</App>"
+    },
+    {
+      code: "<App>{[]}</App>"
+    },
+    {
+      code: "<App>foo</App>"
+    },
+    {
+      code: "<App prop='bar'>foo</App>"
+    },
+    {
+      code: "<App prop='bar'>{'foo \\n bar'}</App>"
+    },
+    {
+      code: "<App prop={'foo \\u00b7 bar'}>foo</App>"
+    },
+    {
+      code: "<MyComponent prop={'bar'}>foo</MyComponent>",
+      options: [{ props: 'always' }],
+    },
+    {
+      code: "<MyComponent>{'foo'}</MyComponent>",
+      options: [{ children: 'always' }],
+    },
+    {
+      code: "<MyComponent>{'foo'}</MyComponent>",
+      options: [{ children: 'ignore' }],
+    },
+    {
+      code: "<MyComponent prop={'bar'}>foo</MyComponent>",
+      options: [{ props: 'ignore' }],
+    },
+    {
+      code: "<MyComponent prop='bar'>foo</MyComponent>",
+      options: [{ props: 'ignore' }],
+    },
+    {
+      code: "<MyComponent>foo</MyComponent>",
+      options: [{ children: 'ignore' }],
+    },
+  ],
+
+  invalid: [
+    {
+      code: "<MyComponent>{'foo'}</MyComponent>",
+      output: "<MyComponent>foo</MyComponent>",
+      errors: [{ message: unnecessaryCurlyMessage }]
+    },
+    {
+      code: "<MyComponent prop={'bar'}>foo</MyComponent>",
+      output: '<MyComponent prop="bar">foo</MyComponent>',
+      errors: [{ message: unnecessaryCurlyMessage }]
+    },
+    {
+      code: "<MyComponent>{'foo'}</MyComponent>",
+      output: "<MyComponent>foo</MyComponent>",
+      options: [{ children: 'never' }],
+      errors: [{ message: unnecessaryCurlyMessage  }]
+    },
+    {
+      code: "<MyComponent prop={'bar'}>foo</MyComponent>",
+      output: '<MyComponent prop="bar">foo</MyComponent>',
+      options: [{ props: 'never' }],
+      errors: [{ message: unnecessaryCurlyMessage }]
+    },
+    {
+      code: "<MyComponent prop='bar'>foo</MyComponent>",
+      options: [{ props: 'always' }],
+      errors: [{ message: missingCurlyMessage }]
+    },
+    {
+      code: "<MyComponent>foo</MyComponent>",
+      options: [{ children: 'always' }],
+      errors: [{ message: missingCurlyMessage }]
+    },
+  ]
+})
