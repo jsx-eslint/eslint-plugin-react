f18b85702e7680c63eba217c84568264421b8bc1
Joachim Seminck <joachim@seminck.be>
2017-09-10 18:11:48 +0300

    Rewrite no-string-refs tests with template literals

--- tests/lib/rules/no-string-refs.js
+++ tests/lib/rules/no-string-refs.js
@@ -30,70 +30,70 @@ const ruleTester = new RuleTester({parserOptions});
 ruleTester.run('no-refs', rule, {
 
   valid: [{
-    code: [
-      'var Hello = createReactClass({',
-      '  componentDidMount: function() {',
-      '     var component = this.hello;',
-      '  },',
-      '  render: function() {',
-      '    return <div ref={c => this.hello = c}>Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentDidMount: function() {
+           var component = this.hello;
+        },
+        render: function() {
+          return <div ref={c => this.hello = c}>Hello {this.props.name}</div>;
+        }
+      });
+    `,
     parser: 'babel-eslint'
   }
   ],
 
   invalid: [{
-    code: [
-      'var Hello = createReactClass({',
-      '  componentDidMount: function() {',
-      '     var component = this.refs.hello;',
-      '  },',
-      '  render: function() {',
-      '    return <div>Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentDidMount: function() {
+           var component = this.refs.hello;
+        },
+        render: function() {
+          return <div>Hello {this.props.name}</div>;
+        }
+      });
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: 'Using this.refs is deprecated.'
     }]
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  render: function() {',
-      '    return <div ref="hello">Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        render: function() {
+          return <div ref="hello">Hello {this.props.name}</div>;
+        }
+      });
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: 'Using string literals in ref attributes is deprecated.'
     }]
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  render: function() {',
-      '    return <div ref={\'hello\'}>Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        render: function() {
+          return <div ref={'hello'}>Hello {this.props.name}</div>;
+        }
+      });
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: 'Using string literals in ref attributes is deprecated.'
     }]
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentDidMount: function() {',
-      '     var component = this.refs.hello;',
-      '  },',
-      '  render: function() {',
-      '    return <div ref="hello">Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentDidMount: function() {
+           var component = this.refs.hello;
+        },
+        render: function() {
+          return <div ref="hello">Hello {this.props.name}</div>;
+        }
+      });
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: 'Using this.refs is deprecated.'
