ed24d082a6e5c833b99cad112cd2d16e05efbc3e
Joachim Seminck <joachim@seminck.be>
2017-09-10 16:23:31 +0300

    Rewrite no-will-update-set-state tests with template literals

--- tests/lib/rules/no-will-update-set-state.js
+++ tests/lib/rules/no-will-update-set-state.js
@@ -30,197 +30,197 @@ const ruleTester = new RuleTester({parserOptions});
 ruleTester.run('no-will-update-set-state', rule, {
 
   valid: [{
-    code: [
-      'var Hello = createReactClass({',
-      '  render: function() {',
-      '    return <div>Hello {this.props.name}</div>;',
-      '  }',
-      '});'
-    ].join('\n')
+    code: `
+      var Hello = createReactClass({
+        render: function() {
+          return <div>Hello {this.props.name}</div>;
+        }
+      });
+    `
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {}',
-      '});'
-    ].join('\n')
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {}
+      });
+    `
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    someNonMemberFunction(arg);',
-      '    this.someHandler = this.setState;',
-      '  }',
-      '});'
-    ].join('\n')
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          someNonMemberFunction(arg);
+          this.someHandler = this.setState;
+        }
+      });
+    `
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    someClass.onSomeEvent(function(data) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    })',
-      '  }',
-      '});'
-    ].join('\n')
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          someClass.onSomeEvent(function(data) {
+            this.setState({
+              data: data
+            });
+          })
+        }
+      });
+    `
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    function handleEvent(data) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    }',
-      '    someClass.onSomeEvent(handleEvent)',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          function handleEvent(data) {
+            this.setState({
+              data: data
+            });
+          }
+          someClass.onSomeEvent(handleEvent)
+        }
+      });
+    `,
     parser: 'babel-eslint'
   }],
 
   invalid: [{
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    this.setState({',
-      '      data: data',
-      '    });',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          this.setState({
+            data: data
+          });
+        }
+      });
+    `,
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'class Hello extends React.Component {',
-      '  componentWillUpdate() {',
-      '    this.setState({',
-      '      data: data',
-      '    });',
-      '  }',
-      '}'
-    ].join('\n'),
+    code: `
+      class Hello extends React.Component {
+        componentWillUpdate() {
+          this.setState({
+            data: data
+          });
+        }
+      }
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    this.setState({',
-      '      data: data',
-      '    });',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          this.setState({
+            data: data
+          });
+        }
+      });
+    `,
     options: ['disallow-in-func'],
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'class Hello extends React.Component {',
-      '  componentWillUpdate() {',
-      '    this.setState({',
-      '      data: data',
-      '    });',
-      '  }',
-      '}'
-    ].join('\n'),
-    parser: 'babel-eslint',
-    options: ['disallow-in-func'],
-    errors: [{
-      message: 'Do not use setState in componentWillUpdate'
-    }]
-  }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    someClass.onSomeEvent(function(data) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    })',
-      '  }',
-      '});'
-    ].join('\n'),
-    options: ['disallow-in-func'],
-    errors: [{
-      message: 'Do not use setState in componentWillUpdate'
-    }]
-  }, {
-    code: [
-      'class Hello extends React.Component {',
-      '  componentWillUpdate() {',
-      '    someClass.onSomeEvent(function(data) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    })',
-      '  }',
-      '}'
-    ].join('\n'),
+    code: `
+      class Hello extends React.Component {
+        componentWillUpdate() {
+          this.setState({
+            data: data
+          });
+        }
+      }
+    `,
     parser: 'babel-eslint',
     options: ['disallow-in-func'],
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    if (true) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    }',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          someClass.onSomeEvent(function(data) {
+            this.setState({
+              data: data
+            });
+          })
+        }
+      });
+    `,
+    options: ['disallow-in-func'],
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'class Hello extends React.Component {',
-      '  componentWillUpdate() {',
-      '    if (true) {',
-      '      this.setState({',
-      '        data: data',
-      '      });',
-      '    }',
-      '  }',
-      '}'
-    ].join('\n'),
-    parser: 'babel-eslint',
-    errors: [{
-      message: 'Do not use setState in componentWillUpdate'
-    }]
-  }, {
-    code: [
-      'var Hello = createReactClass({',
-      '  componentWillUpdate: function() {',
-      '    someClass.onSomeEvent((data) => this.setState({data: data}));',
-      '  }',
-      '});'
-    ].join('\n'),
+    code: `
+      class Hello extends React.Component {
+        componentWillUpdate() {
+          someClass.onSomeEvent(function(data) {
+            this.setState({
+              data: data
+            });
+          })
+        }
+      }
+    `,
     parser: 'babel-eslint',
     options: ['disallow-in-func'],
     errors: [{
       message: 'Do not use setState in componentWillUpdate'
     }]
   }, {
-    code: [
-      'class Hello extends React.Component {',
-      '  componentWillUpdate() {',
-      '    someClass.onSomeEvent((data) => this.setState({data: data}));',
-      '  }',
-      '}'
-    ].join('\n'),
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          if (true) {
+            this.setState({
+              data: data
+            });
+          }
+        }
+      });
+    `,
+    errors: [{
+      message: 'Do not use setState in componentWillUpdate'
+    }]
+  }, {
+    code: `
+      class Hello extends React.Component {
+        componentWillUpdate() {
+          if (true) {
+            this.setState({
+              data: data
+            });
+          }
+        }
+      }
+    `,
+    parser: 'babel-eslint',
+    errors: [{
+      message: 'Do not use setState in componentWillUpdate'
+    }]
+  }, {
+    code: `
+      var Hello = createReactClass({
+        componentWillUpdate: function() {
+          someClass.onSomeEvent((data) => this.setState({data: data}));
+        }
+      });
+    `,
+    parser: 'babel-eslint',
+    options: ['disallow-in-func'],
+    errors: [{
+      message: 'Do not use setState in componentWillUpdate'
+    }]
+  }, {
+    code: `
+      class Hello extends React.Component {
+        componentWillUpdate() {
+          someClass.onSomeEvent((data) => this.setState({data: data}));
+        }
+      }
+    `,
     parser: 'babel-eslint',
     options: ['disallow-in-func'],
     errors: [{
