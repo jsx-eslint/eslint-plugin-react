8681c922d4ddabf57bd0dc7ccb65bacc2c7d86a2
Joachim Seminck <joachim@seminck.be>
2017-09-10 13:32:29 +0300

    Rewrite require-optimization tests with template literals

--- tests/lib/rules/require-optimization.js
+++ tests/lib/rules/require-optimization.js
@@ -21,168 +21,163 @@ const MESSAGE = 'Component is not optimized. Please add a shouldComponentUpdate 
 const ruleTester = new RuleTester({parserOptions});
 ruleTester.run('react-require-optimization', rule, {
   valid: [{
-    code: [
-      'class A {}'
-    ].join('\n')
+    code: `
+      class A {}
+    `
   }, {
-    code: [
-      'import React from "react";' +
-      'class YourComponent extends React.Component {' +
-      'shouldComponentUpdate () {}' +
-      '}'
-    ].join('\n')
+    code: `
+      import React from "react";
+      class YourComponent extends React.Component {
+        shouldComponentUpdate () {}
+      }
+    `
   }, {
-    code: [
-      'import React, {Component} from "react";' +
-      'class YourComponent extends Component {' +
-      'shouldComponentUpdate () {}' +
-      '}'
-    ].join('\n')
+    code: `
+      import React, {Component} from "react";
+      class YourComponent extends Component {
+        shouldComponentUpdate () {}
+      }
+    `
   }, {
-    code: [
-      'import React, {Component} from "react";',
-      '@reactMixin.decorate(PureRenderMixin)',
-      'class YourComponent extends Component {',
-      '  componetnDidMount () {}',
-      '  render() {}',
-      '}'
-    ].join('\n'),
+    code: `
+      import React, {Component} from "react";
+      @reactMixin.decorate(PureRenderMixin)
+      class YourComponent extends Component {
+        componetnDidMount () {}
+        render() {}
+      }
+    `,
     parser: 'babel-eslint'
   }, {
-    code: [
-      'import React from "react";' +
-      'createReactClass({' +
-      'shouldComponentUpdate: function () {}' +
-      '})'
-    ].join('\n')
+    code: `
+      import React from "react";
+      createReactClass({
+        shouldComponentUpdate: function () {}
+      })
+    `
   }, {
-    code: [
-      'import React from "react";' +
-      'createReactClass({' +
-      'mixins: [PureRenderMixin]' +
-      '})'
-    ].join('\n')
+    code: `
+      import React from "react";
+      createReactClass({
+        mixins: [PureRenderMixin]
+      })
+    `
   }, {
-    code: [
-      '@reactMixin.decorate(PureRenderMixin)',
-      'class DecoratedComponent extends Component {' +
-      '}'
-    ].join('\n'),
+    code: `
+      @reactMixin.decorate(PureRenderMixin)
+      class DecoratedComponent extends Component {}
+    `,
     parser: 'babel-eslint'
   }, {
-    code: [
-      'const FunctionalComponent = function (props) {' +
-      'return <div />;' +
-      '}'
-    ].join('\n'),
+    code: `
+      const FunctionalComponent = function (props) {
+        return <div />;
+      }
+    `,
     parser: 'babel-eslint'
   }, {
-    code: [
-      'function FunctionalComponent(props) {' +
-      'return <div />;' +
-      '}'
-    ].join('\n'),
+    code: `
+      function FunctionalComponent(props) {
+        return <div />;
+      }
+    `,
     parser: 'babel-eslint'
   }, {
-    code: [
-      'const FunctionalComponent = (props) => {' +
-      'return <div />;' +
-      '}'
-    ].join('\n'),
+    code: `
+      const FunctionalComponent = (props) => {
+        return <div />;
+      }
+    `,
     parser: 'babel-eslint'
   }, {
-    code: [
-      '@bar',
-      '@pureRender',
-      '@foo',
-      'class DecoratedComponent extends Component {' +
-      '}'
-    ].join('\n'),
+    code: `
+      @bar
+      @pureRender
+      @foo
+      class DecoratedComponent extends Component {}
+    `,
     parser: 'babel-eslint',
     options: [{allowDecorators: ['renderPure', 'pureRender']}]
   }, {
-    code: [
-      'import React from "react";' +
-      'class YourComponent extends React.PureComponent {}'
-    ].join('\n'),
+    code: `
+      import React from "react";
+      class YourComponent extends React.PureComponent {}
+    `,
     parser: 'babel-eslint',
     options: [{allowDecorators: ['renderPure', 'pureRender']}]
   }, {
-    code: [
-      'import React, {PureComponent} from "react";' +
-      'class YourComponent extends PureComponent {}'
-    ].join('\n'),
+    code: `
+      import React, {PureComponent} from "react";
+      class YourComponent extends PureComponent {}
+    `,
     parser: 'babel-eslint',
     options: [{allowDecorators: ['renderPure', 'pureRender']}]
   }],
 
   invalid: [{
-    code: [
-      'import React from "react";' +
-      'class YourComponent extends React.Component {}'
-    ].join('\n'),
+    code: `
+      import React from "react";
+      class YourComponent extends React.Component {}
+    `,
     errors: [{
       message: MESSAGE
     }]
   }, {
-    code: [
-      'import React from "react";',
-      'class YourComponent extends React.Component {',
-      '  handleClick() {}',
-      '  render() {',
-      '    return <div onClick={this.handleClick}>123</div>',
-      '  }',
-      '}'
-    ].join('\n'),
+    code: `
+      import React from "react";
+      class YourComponent extends React.Component {
+        handleClick() {}
+        render() {
+          return <div onClick={this.handleClick}>123</div>
+        }
+      }
+    `,
     parser: 'babel-eslint',
     errors: [{
       message: MESSAGE
     }]
   }, {
-    code: [
-      'import React, {Component} from "react";' +
-      'class YourComponent extends Component {}'
-    ].join('\n'),
+    code: `
+      import React, {Component} from "react";
+      class YourComponent extends Component {}
+    `,
     errors: [{
       message: MESSAGE
     }]
   }, {
-    code: [
-      'import React from "react";' +
-      'createReactClass({' +
-      '})'
-    ].join('\n'),
+    code: `
+      import React from "react";
+      createReactClass({})
+    `,
     errors: [{
       message: MESSAGE
     }]
   }, {
-    code: [
-      'import React from "react";' +
-      'createReactClass({' +
-      'mixins: [RandomMixin]' +
-      '})'
-    ].join('\n'),
+    code: `
+      import React from "react";
+      createReactClass({
+        mixins: [RandomMixin]
+      })
+    `,
     errors: [{
       message: MESSAGE
     }]
   }, {
-    code: [
-      '@reactMixin.decorate(SomeOtherMixin)',
-      'class DecoratedComponent extends Component {' +
-      '}'
-    ].join('\n'),
+    code: `
+      @reactMixin.decorate(SomeOtherMixin)
+      class DecoratedComponent extends Component {}
+    `,
     errors: [{
       message: MESSAGE
     }],
     parser: 'babel-eslint'
   }, {
-    code: [
-      '@bar',
-      '@pure',
-      '@foo',
-      'class DecoratedComponent extends Component {' +
-      '}'
-    ].join('\n'),
+    code: `
+      @bar
+      @pure
+      @foo
+      class DecoratedComponent extends Component {}
+    `,
     errors: [{
       message: MESSAGE
     }],
