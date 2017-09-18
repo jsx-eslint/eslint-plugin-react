d1169d0dc33093703f0355f216d38905f3bc8c8c
Don Abrams <donabrams@invisionapp.com>
2017-09-18 13:35:27 -0600

    add jsx-no-uppercase-data-aria-attributes

---
+++ docs/rules/jsx-no-uppercase-data-aria-attributes.md
@@ -0,0 +1,23 @@
+# Prevent ignored data-* and aria-* attributes (react/jsx-no-uppercase-data-aria-attributes)
+
+In React, all `aria-*` and `data-*` DOM attributes should be lowercased. Otherwise, they will be ignored.
+
+## Rule Details
+
+The following patterns are considered errors:
+
+```jsx
+var Hello = <a data-popOver={true} href="http://example.com/"></a>
+var Hello = <input aria-describedBy="error-message"/>
+```
+
+The following patterns are not considered errors:
+
+```jsx
+var Hello = <a data-pop-over={true} href="http://example.com/"></a>
+var Hello = <input aria-describedby="error-message"/>
+```
+
+## When Not To Use It
+
+If you are not using React, you can disable this rules
---
+++ tests/lib/rules/jsx-no-uppercase-data-aria-attributes.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Forbid data-* and aria-* attributes from being uppercase to support react
+ * @author Don Abrams
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/jsx-no-uppercase-data-aria-attributes');
+const RuleTester = require('eslint').RuleTester;
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true
+  }
+};
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions});
+ruleTester.run('jsx-no-uppercase-data-aria-attributes', rule, {
+  valid: [
+    {code: '<a data-pop-over={true} href="http://example.com/"></a>'},
+    {code: '<input aria-describedby="error-message"/>'}
+  ],
+  invalid: [{
+    code: '<a data-popOver={true} href="http://example.com"></a>',
+    errors: [{
+      message: 'In React, data-* and aria-* attributes must be lowercase' +
+          ' as per https://facebook.github.io/react/docs/dom-elements.html'
+    }]
+  }, {
+    code: '<input aria-describedBy="error-message"/>',
+    errors: [{
+      message: 'In React, data-* and aria-* attributes must be lowercase' +
+          ' as per https://facebook.github.io/react/docs/dom-elements.html'
+    }]
+  }]
+});
