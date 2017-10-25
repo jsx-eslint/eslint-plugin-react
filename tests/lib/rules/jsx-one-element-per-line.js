618bdb7c5a0ade4ad1face7d3af8f5caae2b7c26
TSMMark <mark.allen@vydia.com>
2017-10-24 23:31:21 -0400

    new rule: one-element-per-line

---
+++ tests/lib/rules/jsx-one-element-per-line.js
@@ -0,0 +1,246 @@
+/**
+ * @fileoverview Limit to one element tag per line in JSX
+ * @author Mark Allen
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/jsx-one-element-per-line');
+const RuleTester = require('eslint').RuleTester;
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true
+  }
+};
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions});
+ruleTester.run('jsx-max-elements-per-line', rule, {
+  valid: [{
+    code: '<App />'
+  }, {
+    code: '<App></App>'
+  }, {
+    code: '<App>{"foo"}</App>'
+  }, {
+    code: '<App>foo</App>'
+  }, {
+    code: '<App foo="bar" />'
+  }, {
+    code: [
+      '<App>',
+      '  <Foo />',
+      '</App>'
+    ].join('\n')
+  }, {
+    code: [
+      '<App>',
+      '  <Foo></Foo>',
+      '</App>'
+    ].join('\n')
+  }, {
+    code: [
+      '<App>',
+      '  <Foo>',
+      '  </Foo>',
+      '</App>'
+    ].join('\n')
+  }, {
+    code: [
+      '<App',
+      '  foo="bar"',
+      '>',
+      '<Foo />',
+      '</App>'
+    ].join('\n')
+  }, {
+    code: [
+      '<',
+      'App',
+      '>',
+      '  <',
+      '    Foo',
+      '  />',
+      '</',
+      'App',
+      '>'
+    ].join('\n')
+  }],
+
+  invalid: [{
+    code: [
+      '<App>',
+      '  <Foo /><Bar />',
+      '</App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '  <Foo />',
+      '<Bar />',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Opening tag for Element `Bar` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '  <Foo></Foo><Bar></Bar>',
+      '</App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '  <Foo></Foo>',
+      '<Bar></Bar>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Opening tag for Element `Bar` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '<Foo></Foo></App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '<Foo></Foo>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App><Foo />',
+      '</App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '<Foo />',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Opening tag for Element `Foo` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '<Foo/></App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '<Foo/>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App><Foo',
+      '/>',
+      '</App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '<Foo',
+      '/>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Opening tag for Element `Foo` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App',
+      '>',
+      '<Foo /></App>'
+    ].join('\n'),
+    output: [
+      '<App',
+      '>',
+      '<Foo />',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App',
+      '>',
+      '<Foo',
+      '/></App>'
+    ].join('\n'),
+    output: [
+      '<App',
+      '>',
+      '<Foo',
+      '/>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App',
+      '><Foo />',
+      '</App>'
+    ].join('\n'),
+    output: [
+      '<App',
+      '>',
+      '<Foo />',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Opening tag for Element `Foo` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '  <Foo></Foo',
+      '></App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '  <Foo></Foo',
+      '>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '  <Foo></',
+      'Foo></App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '  <Foo></',
+      'Foo>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }, {
+    code: [
+      '<App>',
+      '  <Foo></',
+      'Foo></App>'
+    ].join('\n'),
+    output: [
+      '<App>',
+      '  <Foo></',
+      'Foo>',
+      '</App>'
+    ].join('\n'),
+    errors: [{message: 'Closing tag for Element `App` must be placed on a new line'}],
+    parserOptions: parserOptions
+  }]
+});
