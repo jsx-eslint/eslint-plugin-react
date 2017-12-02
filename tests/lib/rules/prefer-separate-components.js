8dd536e981fdca69e5a253ec5025d02620df1247
Joshua Stiefer <facedelajunk@gmail.com>
2017-12-02 12:24:55 -0700

    Add first iteration of prefer-separate-components

---
+++ tests/lib/rules/prefer-separate-components.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview Report usages of class methods besides render returning JSX
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/prefer-separate-components');
+const RuleTester = require('eslint').RuleTester;
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true
+  }
+};
+
+const ruleTester = new RuleTester({parserOptions});
+ruleTester.run('prefer-separate-components', rule, {
+  valid: [{
+    code: `
+      class Foo extends React.Component {
+        render() {
+          return <div>foo</div>;
+        }
+      }
+    `
+  }],
+  invalid: [{
+    code: `
+      class Foo extends React.Component {
+        renderBar() {
+          return <span>bar</span>;
+        }
+        render() {
+          return (
+            <div>
+              foo
+              {this.renderBar()}
+            </div>
+          );
+        }
+      }
+    `,
+    errors: 1
+  }]
+});
