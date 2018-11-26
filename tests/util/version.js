6d9e7bb7a4ed8532827a18055eeb30b8c0c8fa96
Jordan Harband <ljharb@gmail.com>
2018-11-26 13:58:16 -0800

    [Fix] ensure that react and flow versions can be numbers
    Fixes #2056.

--- tests/util/version.js
+++ tests/util/version.js
@@ -31,4 +31,20 @@ describe('Version', () => {
       assert.equal(versionUtil.testReactVersion(context, '999.999.999'), true);
     });
   });
+
+  describe('non-string version', () => {
+    const context = {settings: {react: {version: 15.0, flowVersion: 1.2}}};
+
+    it('works with react', () => {
+      assert.equal(versionUtil.testReactVersion(context, '0.14.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '15.0.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '16.0.0'), false);
+    });
+
+    it('works with flow', () => {
+      assert.equal(versionUtil.testFlowVersion(context, '1.1.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '1.2.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '1.3.0'), false);
+    });
+  });
 });
