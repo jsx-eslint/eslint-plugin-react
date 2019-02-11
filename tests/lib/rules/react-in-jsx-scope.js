2098434ef0076a6525a4e33affa2c54d700200d7
Junyoung Clare Jang <jjc9310@gmail.com>
2019-02-11 15:38:39 -0500

    Add jsxFragPragma support to react-in-jsx-scope rule

--- tests/lib/rules/react-in-jsx-scope.js
+++ tests/lib/rules/react-in-jsx-scope.js
@@ -20,12 +20,6 @@ const parserOptions = {
   }
 };
 
-const settings = {
-  react: {
-    pragma: 'Foo'
-  }
-};
-
 // -----------------------------------------------------------------------------
 // Tests
 // -----------------------------------------------------------------------------
@@ -35,47 +29,81 @@ ruleTester.run('react-in-jsx-scope', rule, {
   valid: [
     {code: 'var React, App; <App />;'},
     {code: 'var React; <img />;'},
-    {code: 'var React; <>fragment</>;', parser: 'babel-eslint'},
+    {
+      code: 'var React; <>fragment</>;',
+      parser: 'babel-eslint'
+    },
     {code: 'var React; <x-gif />;'},
     {code: 'var React, App, a=1; <App attr={a} />;'},
     {code: 'var React, App, a=1; function elem() { return <App attr={a} />; }'},
     {code: 'var React, App; <App />;'},
     {code: '/** @jsx Foo */ var Foo, App; <App />;'},
     {code: '/** @jsx Foo.Bar */ var Foo, App; <App />;'},
-    {code: `
-      import React from 'react/addons';
-      const Button = createReactClass({
-        render() {
-          return (
-            <button {...this.props}>{this.props.children}</button>
-          )
+    {
+      code: '/** @jsxFrag Foo.Bar */ var Foo; <></>;',
+      parser: 'babel-eslint'
+    },
+    {
+      code: `
+        import React from 'react/addons';
+        const Button = createReactClass({
+          render() {
+            return (
+              <button {...this.props}>{this.props.children}</button>
+            )
+          }
+        });
+        export default Button;
+      `
+    },
+    {
+      code: 'var Foo; <App />;',
+      settings: {
+        react: {
+          jsxPragma: 'Foo'
         }
-      });
-      export default Button;
-    `},
-    {code: 'var Foo, App; <App />;', settings: settings}
+      }
+    }
   ],
-  invalid: [{
-    code: 'var App, a = <App />;',
-    errors: [{message: '\'React\' must be in scope when using JSX'}]
-  }, {
-    code: 'var a = <App />;',
-    errors: [{message: '\'React\' must be in scope when using JSX'}]
-  }, {
-    code: 'var a = <img />;',
-    errors: [{message: '\'React\' must be in scope when using JSX'}]
-  }, {
-    code: 'var a = <>fragment</>;',
-    parser: 'babel-eslint',
-    errors: [{message: '\'React\' must be in scope when using JSX'}]
-  }, {
-    code: '/** @jsx React.DOM */ var a = <img />;',
-    errors: [{message: '\'React\' must be in scope when using JSX'}]
-  }, {
-    code: '/** @jsx Foo.bar */ var React, a = <img />;',
-    errors: [{message: '\'Foo\' must be in scope when using JSX'}]
-  }, {
-    code: 'var React, a = <img />;',
-    errors: [{message: '\'Foo\' must be in scope when using JSX'}], settings: settings
-  }]
+  invalid: [
+    {
+      code: 'var App, a = <App />;',
+      errors: [{message: '\'React\' must be in scope when using JSX'}]
+    },
+    {
+      code: 'var a = <App />;',
+      errors: [{message: '\'React\' must be in scope when using JSX'}]
+    },
+    {
+      code: 'var a = <img />;',
+      errors: [{message: '\'React\' must be in scope when using JSX'}]
+    },
+    {
+      code: 'var a = <>fragment</>;',
+      parser: 'babel-eslint',
+      errors: [{message: '\'React\' must be in scope when using JSX'}]
+    },
+    {
+      code: '/** @jsx React.DOM */ var a = <img />;',
+      errors: [{message: '\'React\' must be in scope when using JSX'}]
+    },
+    {
+      code: '/** @jsx Foo.bar */ var React, a = <img />;',
+      errors: [{message: '\'Foo\' must be in scope when using JSX'}]
+    },
+    {
+      code: '/** @jsxFrag Foo.bar */ var React; <></>;',
+      parser: 'babel-eslint',
+      errors: [{message: '\'Foo\' must be in scope when using JSX'}]
+    },
+    {
+      code: 'var React, a = <img />;',
+      errors: [{message: '\'Foo\' must be in scope when using JSX'}],
+      settings: {
+        react: {
+          jsxPragma: 'Foo.createElement'
+        }
+      }
+    }
+  ]
 });
