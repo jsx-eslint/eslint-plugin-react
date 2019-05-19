e14f2b839fcdd252fa65135daa8ad5a3af5eee70
Jordan Harband <ljharb@gmail.com>
2019-05-18 22:26:59 -0700

    [eslint] enable and autofix `arrow-parens`

--- tests/index.js
+++ tests/index.js
@@ -12,7 +12,7 @@ const ruleFiles = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
   .map(f => path.basename(f, '.js'));
 
 describe('all rule files should be exported by the plugin', () => {
-  ruleFiles.forEach(ruleName => {
+  ruleFiles.forEach((ruleName) => {
     it(`should export ${ruleName}`, () => {
       assert.equal(
         plugin.rules[ruleName],
@@ -24,7 +24,7 @@ describe('all rule files should be exported by the plugin', () => {
 
 describe('deprecated rules', () => {
   it('marks all deprecated rules as deprecated', () => {
-    ruleFiles.forEach(ruleName => {
+    ruleFiles.forEach((ruleName) => {
       const inDeprecatedRules = Boolean(plugin.deprecatedRules[ruleName]);
       const isDeprecated = plugin.rules[ruleName].meta.deprecated;
       if (inDeprecatedRules) {
@@ -39,13 +39,13 @@ describe('deprecated rules', () => {
 describe('configurations', () => {
   it('should export a ‘recommended’ configuration', () => {
     assert(plugin.configs.recommended);
-    Object.keys(plugin.configs.recommended.rules).forEach(configName => {
+    Object.keys(plugin.configs.recommended.rules).forEach((configName) => {
       assert.equal(configName.indexOf('react/'), 0);
       const ruleName = configName.slice('react/'.length);
       assert(plugin.rules[ruleName]);
     });
 
-    ruleFiles.forEach(ruleName => {
+    ruleFiles.forEach((ruleName) => {
       const inRecommendedConfig = !!plugin.configs.recommended.rules[`react/${ruleName}`];
       const isRecommended = plugin.rules[ruleName].meta.docs.recommended;
       if (inRecommendedConfig) {
@@ -59,12 +59,12 @@ describe('configurations', () => {
   it('should export an ‘all’ configuration', () => {
     assert(plugin.configs.all);
 
-    Object.keys(plugin.configs.all.rules).forEach(configName => {
+    Object.keys(plugin.configs.all.rules).forEach((configName) => {
       assert.equal(configName.indexOf('react/'), 0);
       assert.equal(plugin.configs.all.rules[configName], 2);
     });
 
-    ruleFiles.forEach(ruleName => {
+    ruleFiles.forEach((ruleName) => {
       const inDeprecatedRules = Boolean(plugin.deprecatedRules[ruleName]);
       const inAllConfig = Boolean(plugin.configs.all.rules[`react/${ruleName}`]);
       assert(inDeprecatedRules ^ inAllConfig); // eslint-disable-line no-bitwise
