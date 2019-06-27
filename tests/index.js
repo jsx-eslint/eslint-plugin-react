d6832d66756a24a3fd11b96705df2e4fa43fdf3f
Yannick Croissant <yannick.croissant@gmail.com>
2019-06-27 10:39:06 +0200

    Fix new required import order

--- tests/index.js
+++ tests/index.js
@@ -2,11 +2,11 @@
 
 'use strict';
 
-const plugin = require('..');
-
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+
+const plugin = require('..');
 
 const ruleFiles = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
   .map(f => path.basename(f, '.js'));
