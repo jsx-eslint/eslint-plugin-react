da7a0458d46d88ba33a81b8806edd94313077e29
Chiawen Chen <golopot@gmail.com>
2020-03-08 18:39:06 +0800

    [Fix] `no-unused-state`: handle optional chaining
    Fixes #2515

--- tests/helpers/parsers.js
+++ tests/helpers/parsers.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const path = require('path');
+const semver = require('semver');
+const version = require('eslint/package.json').version;
 
 const NODE_MODULES = '../../node_modules';
 
 module.exports = {
   BABEL_ESLINT: path.join(__dirname, NODE_MODULES, 'babel-eslint'),
   TYPESCRIPT_ESLINT: path.join(__dirname, NODE_MODULES, 'typescript-eslint-parser'),
-  '@TYPESCRIPT_ESLINT': path.join(__dirname, NODE_MODULES, '@typescript-eslint/parser')
+  '@TYPESCRIPT_ESLINT': path.join(__dirname, NODE_MODULES, '@typescript-eslint/parser'),
+  TS: function TS(tests) {
+    if (semver.satisfies(version, '>= 5')) {
+      return tests;
+    }
+    return [];
+  }
 };
