b5d24c580e3c4f45939ccf713bbdbe40e09fc47b
Jordan Harband <ljharb@gmail.com>
2022-02-04 16:23:54 -0800

    [Refactor] `versionUtil`: expand to take a semver range
    This would be breaking, but this util isn't part of our API.

--- tests/util/version.js
+++ tests/util/version.js
@@ -34,25 +34,25 @@ describe('Version', () => {
     it('matches detected version', () => {
       sinon.stub(context, 'getFilename').callsFake(() => path.resolve(base, 'detect-version', 'test.js'));
 
-      assert.equal(versionUtil.testReactVersion(context, '1.2.3'), true);
-      assert.equal(versionUtil.testReactVersion(context, '1.2.4'), false);
-      assert.equal(versionUtil.testFlowVersion(context, '0.92.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 1.2.3'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 1.2.4'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 0.92.0'), true);
     });
 
     it('matches detected version in sibling project', () => {
       sinon.stub(context, 'getFilename').callsFake(() => path.resolve(base, 'detect-version-sibling', 'test.js'));
 
-      assert.equal(versionUtil.testReactVersion(context, '2.3.4'), true);
-      assert.equal(versionUtil.testReactVersion(context, '2.3.5'), false);
-      assert.equal(versionUtil.testFlowVersion(context, '2.92.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.4'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.5'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 2.92.0'), true);
     });
 
     it('matches detected version in child project', () => {
       sinon.stub(context, 'getFilename').callsFake(() => path.resolve(base, 'detect-version', 'detect-version-child', 'test.js'));
 
-      assert.equal(versionUtil.testReactVersion(context, '3.4.5'), true);
-      assert.equal(versionUtil.testReactVersion(context, '3.4.6'), false);
-      assert.equal(versionUtil.testFlowVersion(context, '3.92.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 3.4.5'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 3.4.6'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 3.92.0'), true);
     });
 
     it('assumes latest version if react is not installed', () => {
@@ -87,17 +87,17 @@ describe('Version', () => {
     it('works with virtual filename', () => {
       sinon.stub(context, 'getFilename').callsFake(() => path.resolve(base, 'detect-version-sibling', 'test.js/0_fake.js'));
 
-      assert.equal(versionUtil.testReactVersion(context, '2.3.4'), true);
-      assert.equal(versionUtil.testReactVersion(context, '2.3.5'), false);
-      assert.equal(versionUtil.testFlowVersion(context, '2.92.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.4'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.5'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 2.92.0'), true);
     });
 
     it('works with recursive virtual filename', () => {
       sinon.stub(context, 'getFilename').callsFake(() => path.resolve(base, 'detect-version-sibling', 'test.js/0_fake.md/1_fake.js'));
 
-      assert.equal(versionUtil.testReactVersion(context, '2.3.4'), true);
-      assert.equal(versionUtil.testReactVersion(context, '2.3.5'), false);
-      assert.equal(versionUtil.testFlowVersion(context, '2.92.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.4'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 2.3.5'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 2.92.0'), true);
     });
   });
 
@@ -105,15 +105,15 @@ describe('Version', () => {
     const context = { settings: { react: { version: '15.0', flowVersion: '1.2' } } };
 
     it('works with react', () => {
-      assert.equal(versionUtil.testReactVersion(context, '0.14.0'), true);
-      assert.equal(versionUtil.testReactVersion(context, '15.0.0'), true);
-      assert.equal(versionUtil.testReactVersion(context, '16.0.0'), false);
+      assert.equal(versionUtil.testReactVersion(context, '>= 0.14.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 15.0.0'), true);
+      assert.equal(versionUtil.testReactVersion(context, '>= 16.0.0'), false);
     });
 
     it('works with flow', () => {
-      assert.equal(versionUtil.testFlowVersion(context, '1.1.0'), true);
-      assert.equal(versionUtil.testFlowVersion(context, '1.2.0'), true);
-      assert.equal(versionUtil.testFlowVersion(context, '1.3.0'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.1.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.2.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.3.0'), false);
     });
   });
 
@@ -121,9 +121,9 @@ describe('Version', () => {
     const context = { settings: { react: { version: 15.0, flowVersion: 1.2 } } };
 
     it('works with react', () => {
-      assert.equal(versionUtil.testReactVersion(context, '0.14.0'), true);
-      assert.equal(versionUtil.testReactVersion(context, '15.0.0'), true);
-      assert.equal(versionUtil.testReactVersion(context, '16.0.0'), false);
+      assert.equal(versionUtil.testReactVersion(context, '>= 0.14.0'), true, '>= 0.14.0');
+      assert.equal(versionUtil.testReactVersion(context, '>= 15.0.0'), true, '>= 15.0.0');
+      assert.equal(versionUtil.testReactVersion(context, '>= 16.0.0'), false, '>= 16.0.0');
 
       expectedErrorArgs = [
         ['Warning: React version specified in eslint-plugin-react-settings must be a string; got “number”'],
@@ -133,9 +133,9 @@ describe('Version', () => {
     });
 
     it('works with flow', () => {
-      assert.equal(versionUtil.testFlowVersion(context, '1.1.0'), true);
-      assert.equal(versionUtil.testFlowVersion(context, '1.2.0'), true);
-      assert.equal(versionUtil.testFlowVersion(context, '1.3.0'), false);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.1.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.2.0'), true);
+      assert.equal(versionUtil.testFlowVersion(context, '>= 1.3.0'), false);
 
       expectedErrorArgs = [
         ['Warning: Flow version specified in eslint-plugin-react-settings must be a string; got “number”'],
