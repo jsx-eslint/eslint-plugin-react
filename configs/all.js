2124d13297d0c9547ec4383160147410c30a5cfd
Brad Zacher <brad.zacher@gmail.com>
2024-02-22 09:29:21 +1030

    [Fix] add a few jsdoc type annotations to work around TS inference for consumers

--- configs/all.js
+++ configs/all.js
@@ -9,6 +9,10 @@ function filterRules(rules, predicate) {
   return fromEntries(entries(rules).filter((entry) => predicate(entry[1])));
 }
 
+/**
+ * @param {object} rules - rules object mapping rule name to rule module
+ * @returns {Record<string, 2>}
+ */
 function configureAsError(rules) {
   return fromEntries(Object.keys(rules).map((key) => [`react/${key}`, 2]));
 }
@@ -20,6 +24,12 @@ const deprecatedRules = filterRules(allRules, (rule) => rule.meta.deprecated);
 
 module.exports = {
   plugins: {
+    /**
+     * @type {{
+     *   deprecatedRules: Record<string, import('eslint').Rule.RuleModule>,
+     *   rules: Record<string, import('eslint').Rule.RuleModule>,
+     * }}
+     */
     react: {
       deprecatedRules,
       rules: allRules,
